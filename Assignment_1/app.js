c5b10fd0e424429ae51db816f56062bf5ebd5d40
Marianna Taavitsainen <marianna.taavitsainen@gmail.com>
2026-03-12 13:53:04 +0200

    Personoitu express-lausetta

--- Assignment_1/app.js
+++ Assignment_1/app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Welcome to check my Render-hosted Express-page')
 })
 
 app.listen(port, () => {
